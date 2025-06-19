3a0d9e31d0edad8fec5d472ec32b6a3e1a8c6910
wangzc1233 <wangzc025@163.com>
2025-06-19 17:14:27 +0800

    Update requirement.sh

--- prot2chat/requirement.sh
+++ prot2chat/requirement.sh
@@ -1,5 +1,10 @@
-pip install torch torchvision torchaudio
-pip install modelscope \
-            peft \
-            nltk \
-            biotite-structure
+pip3 install torch torchvision torchaudio 
+pip install flask
+pip install langchain==0.2.5
+pip install langchain-community==0.2.5
+pip install transformers==4.50.3
+pip install modelscope==1.24.1
+pip install biotite  #==0.41.2
+pip install numpy==1.26.4
+pip install accelerate==0.31.0
+pip install peft==0.11.1
